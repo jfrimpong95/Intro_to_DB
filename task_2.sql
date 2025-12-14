9243629aa2ac112c1b0073927489ab3c339aa305
Frimpong Joseph <josephfrimpong91@gmail.com>
2025-12-14 18:59:41 +0000

    Reformat SQL script to add missing Customers and Orders tables

--- task_2.sql
+++ task_2.sql
@@ -19,4 +19,26 @@ CREATE TABLE IF NOT EXISTS Books (
 
 -- CUSTOMERS TABLE
 CREATE TABLE IF NOT EXISTS Customers (
-    customer_id INT AUTO_I
+    customer_id INT AUTO_INCREMENT PRIMARY KEY,
+    customer_name VARCHAR(215),
+    email VARCHAR(215),
+    address TEXT
+);
+
+-- ORDERS TABLE
+CREATE TABLE IF NOT EXISTS Orders (
+    order_id INT AUTO_INCREMENT PRIMARY KEY,
+    customer_id INT,
+    order_date DATE,
+    FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
+);
+
+-- ORDER DETAILS TABLE
+CREATE TABLE IF NOT EXISTS Order_Details (
+    orderdetailid INT AUTO_INCREMENT PRIMARY KEY,
+    order_id INT,
+    book_id INT,
+    quantity DOUBLE,
+    FOREIGN KEY (order_id) REFERENCES Orders(order_id),
+    FOREIGN KEY (book_id) REFERENCES Books(book_id)
+);
